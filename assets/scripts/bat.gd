481cbf80c2bef8522e3d756b86bc8a477f853d67
camheath <166544172+camheath@users.noreply.github.com>
2024-07-21 15:56:30 -0600

    Bat Murder

--- assets/scripts/bat.gd
+++ assets/scripts/bat.gd
@@ -8,3 +8,10 @@ func _physics_process(delta):
 	position.x -= batSpeed
 	if position.x < -100:
 		queue_free()
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is PlayerProjectile:
+		area.queue_free()
+		queue_free()
--- assets/scripts/fireball.gd
+++ assets/scripts/fireball.gd
@@ -1,7 +1,7 @@
 extends Area2D
 class_name  PlayerProjectile
 
-var speed = 5
+var speed = 7
 
 func _physics_process(delta):
 	position.x += speed
